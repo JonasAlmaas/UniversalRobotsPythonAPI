cac6616a18f9db02edcbfea1e45a5b0c19f22817
JonasAlmaas <44512227+JonasAlmaas@users.noreply.github.com>
2022-02-22 14:56:42 +0100

    Add camera pose

--- example_robot_movement.py
+++ example_robot_movement.py
@@ -5,18 +5,11 @@ class Pose:
     pos1 = ur.Pose(x= 860, y=-216, z=1130, rx=2.4, ry=-1.7, rz=2.6)
     pos2 = ur.Pose(x= 860, y=-216, z=800, rx=2.4, ry=-1.7, rz=2.6)
 
+    camera = ur.Pose(1003, -180, 1244, -1.48, 1.0, -1.51)
 
 robot = ur.UniversalRobot()
 
-robot.set_accel(1.5)
-robot.set_vel(1.5)
-
-print("Before movement 1")
-robot.move_to(target_pose=Pose.pos1, wait=True)
-print("After movment 2")
-
-print("Before movement 2")
-robot.move_to(target_pose=Pose.pos2, wait=False)
-print("After movment 2")
+# robot.move_to(target_pose=Pose.pos1, wait=True)
+robot.move_to(target_pose=Pose.camera, wait=True)
 
 # robot.set_forcemove(state=True)
