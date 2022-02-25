a07a4f33a786c6c5525c3b68a99f497d08c54837
JonasAlmaas <44512227+JonasAlmaas@users.noreply.github.com>
2022-02-25 14:33:37 +0100

    Update example_robot_movement.py

--- scripts/example_robot_movement.py
+++ scripts/example_robot_movement.py
@@ -11,7 +11,7 @@ class Pose:
     pose1 = urpy.Pose(x=1060, y=-216, z=800, rx=-1.44, ry=1.02, rz=-1.56)
     pose2 = urpy.Pose(x= 860, y=-216, z=1130, rx=2.4, ry=-1.7, rz=2.6)
     # camera = urpy.Pose(1003, -180, 1244, -1.48, 1.0, -1.51)
-    camera = urpy.Pose(853, -180, 1140, -1.48, 1.0, -1.51)
+    camera = urpy.Pose(853, -180, 1220, -1.48, 1.0, -1.51)
 
 
 robot = urpy.UniversalRobot("192.168.1.101")
