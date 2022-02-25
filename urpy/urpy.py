326e8e9a62c9c7672314f106ba53f9e6de354993
JonasAlmaas <jonasalmaas7@gmail.com>
2022-02-25 20:56:07 +0100

    Massive clean up

--- urpy/urpy.py
+++ urpy/urpy.py
@@ -18,80 +18,18 @@ CONFIG = "configuration.xml"
 
 
 class MovementType(Enum):
+    '''Defined how the robot should move.'''
     LINEAR = auto()
     QUICKEST = auto()
 
 
-def lerp(a, b, percent) -> float:
-    return a + ((b - a) * percent)
-
-
-class Pose:
-    '''A wrapper around an arm pose.'''
-    def __init__(self, x=0.0, y=0.0, z=0.0, rx=0.0, ry=0.0, rz=0.0):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.rx = round(rx, 2)
-        self.ry = round(ry, 2)
-        self.rz = round(rz, 2)
-
-    def to_m(self):
-        '''Converts from mm to m.'''
-        self.x = self.x / 1000.0
-        self.y = self.y / 1000.0
-        self.z = self.z / 1000.0
-    
-    def to_mm(self):
-        '''Converts from m to mm.'''
-        self.x = round(self.x * 1000.0, 1)
-        self.y = round(self.y * 1000.0, 1)
-        self.z = round(self.z * 1000.0, 1)
-
-    def to_declaration(self):
-        '''Returns the declaration of the pose.'''
-        return "Pose(x=" + str(self.x) + ", y=" + str(self.y) + ", z=" + str(self.z) + ", rx=" + str(self.rx) + ", ry=" + str(self.ry) + ", rz=" + str(self.rz) + ")"
-
-    def get_undefined_move_command(self, a, v):
-        return "p[" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ", " + str(self.rx) + ", " + str(self.ry) + ", " + str(self.rz) + "],a=" + str(a) + ", v=" +str(v)
-
-    def movej(self, a, v) -> str:
-        '''Returns a string for the pose.'''
-        return "movej(" + self.get_undefined_move_command(a, v) + ")\n"
-
-    def movel(self, a, v) -> str:
-        '''Returns a string for the pose.'''
-        return "movel(" + self.get_undefined_move_command(a, v) + ")\n"
-    
-    def lerp(self, other: Pose, percent: float) -> Pose:
-        '''Returns a linear interpolated pose.'''
-        x = lerp(self.x, other.x, percent)
-        y = lerp(self.y, other.y, percent)
-        z = lerp(self.z, other.z, percent)
-        rx = lerp(self.rx, other.rx, percent)
-        ry = lerp(self.ry, other.ry, percent)
-        rz = lerp(self.rz, other.rz, percent)
-        return Pose(x, y, z, rx, ry, rz)
-
-    def copy(self) -> Pose:
-        return Pose(x=self.x, y=self.y, z=self.z, rx=self.rx, ry=self.ry, rz=self.rz)
-
-    def __eq__(self, other):
-        if (isinstance(other, Pose)):
-            # return (self.x == other.x) and (self.y == other.y) and (self.z == other.z) and (self.rx == other.rx) and (self.ry == other.ry) and (self.rz == other.rz)
-            return (self.x == other.x) and (self.y == other.y) and (self.z == other.z)
-        return False
-    
-    def __str__(self):
-        return "X: " + str(self.x) + ", Y: " + str(self.y) + ", Z: " + str(self.z) + ", rX: " + str(self.rx) + ", rY: " + str(self.ry) + ", rZ: " + str(self.rz)
-
-
 class UniversalRobot:
 
-    def __init__(self, host_ip: str):
-        self._host = host_ip
-        self._accel = 1.5
-        self._vel = 1.5
+    def __init__(self, host_ip: str) -> None:
+        '''A helper class to comunicate with a Universal Robot.'''
+        self._host: str = host_ip
+        self._accel: float = 1.5
+        self._vel: float = 1.5
 
         parser = argparse.ArgumentParser()
         parser.add_argument('--host', default=self._host,help='name of host to connect to (localhost)')
@@ -107,21 +45,22 @@ class UniversalRobot:
         if self._args.verbose:
             logging.basicConfig(level=logging.INFO)
 
-    def send_to_robot(self, function_str: str):
+    def send_to_robot(self, function_str: str) -> None:
+        '''Sends a function string to the robot.'''
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((self._host, PORT_SEND))
         s.send(function_str.encode())
         s.close()
 
-    def set_accel(self, accel: float):
+    def set_accel(self, accel: float) -> None:
         '''Sets the acceleration for the robot.'''
         self._accel = accel
     
-    def set_vel(self, vel: float):
+    def set_vel(self, vel: float) -> None:
         '''Sets the velcoity for the robot.'''
         self._vel = vel
 
-    def move_to(self, target_pose: Pose, movement_type: MovementType = MovementType.QUICKEST, wait=True):
+    def move_to(self, target_pose: Pose, movement_type: MovementType = MovementType.QUICKEST, wait: bool = True) -> None:
         '''Moves the robot to the desiered pose, waits before continuing if the 'wait' flag is set.'''
         target_pose.to_m()
 
@@ -130,7 +69,7 @@ class UniversalRobot:
         elif movement_type is MovementType.QUICKEST:
             self.send_to_robot(target_pose.movej(self._accel, self._vel))
         else:
-            print("Unknown movement type")
+            print("[urpy][ERROR]: Unknown movement type.")
             return
 
         if wait:
@@ -144,7 +83,7 @@ class UniversalRobot:
                     time.sleep(0.1)
 
     def get_pose(self) -> Pose:
-        '''Get the robots correct pose as a struct. Example: pose.x, pose.rx'''
+        '''Get the robots correct pose as an instance of the Pose class.'''
         conf = rtde_config.ConfigFile(self._args.config)
         output_names, output_types = conf.get_recipe('out')
 
@@ -163,7 +102,7 @@ class UniversalRobot:
         if state is not None:
             x, y, z, rx, ry, rz = state.actual_TCP_pose
         else:
-            print("Something went wrong!")
+            print("[urpy][ERROR]: Failed to get pose!")
 
         con.send_pause()
         con.disconnect()
@@ -173,7 +112,7 @@ class UniversalRobot:
 
         return pose
     
-    def set_freedrive(self, state=True):
+    def set_freedrive(self, state=True) -> None:
         '''Sets the robot in freedrive mode until another request is sent.'''
         function_str = None
 
@@ -183,9 +122,70 @@ class UniversalRobot:
             function_str = "end_freedrive_mode()\n"
 
         self.send_to_robot(function_str)
+
+
+class Pose:
+    '''A wrapper around an arm pose.'''
+    def __init__(self, x: float = 0.0, y: float = 0.0, z: float = 0.0, rx: float = 0.0, ry: float = 0.0, rz: float = 0.0) -> None:
+        self.x: float = x
+        self.y: float = y
+        self.z: float = z
+        self.rx: float = round(rx, 2)
+        self.ry: float = round(ry, 2)
+        self.rz: float = round(rz, 2)
+
+    def to_m(self) -> None:
+        '''Converts from mm to m.'''
+        self.x = self.x / 1000.0
+        self.y = self.y / 1000.0
+        self.z = self.z / 1000.0
     
-    def set_forcemode(self, state):
-        if state:
-            pass
-        else:
-            pass
+    def to_mm(self) -> None:
+        '''Converts from m to mm.'''
+        self.x = round(self.x * 1000.0, 1)
+        self.y = round(self.y * 1000.0, 1)
+        self.z = round(self.z * 1000.0, 1)
+
+    def to_declaration(self) -> str:
+        '''Returns the declaration of the pose.'''
+        return "Pose(x=" + str(self.x) + ", y=" + str(self.y) + ", z=" + str(self.z) + ", rx=" + str(self.rx) + ", ry=" + str(self.ry) + ", rz=" + str(self.rz) + ")"
+
+    def _get_undefined_move_command(self, a, v) -> str:
+        '''Helper function for sending a pose to the robot.'''
+        return "p[" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ", " + str(self.rx) + ", " + str(self.ry) + ", " + str(self.rz) + "],a=" + str(a) + ", v=" +str(v)
+
+    def movej(self, a, v) -> str:
+        '''Returns a function string for the pose.'''
+        return "movej(" + self._get_undefined_move_command(a, v) + ")\n"
+
+    def movel(self, a, v) -> str:
+        '''Returns a function string for the pose.'''
+        return "movel(" + self._get_undefined_move_command(a, v) + ")\n"
+    
+    def lerp(self, other: Pose, percent: float) -> Pose:
+        '''Returns a linear interpolated pose.'''
+        x = lerp(self.x, other.x, percent)
+        y = lerp(self.y, other.y, percent)
+        z = lerp(self.z, other.z, percent)
+        rx = lerp(self.rx, other.rx, percent)
+        ry = lerp(self.ry, other.ry, percent)
+        rz = lerp(self.rz, other.rz, percent)
+        return Pose(x, y, z, rx, ry, rz)
+
+    def copy(self) -> Pose:
+        '''Returns a copy of the pose.'''
+        return Pose(x=self.x, y=self.y, z=self.z, rx=self.rx, ry=self.ry, rz=self.rz)
+
+    def __eq__(self, other):
+        if (isinstance(other, Pose)):
+            # return (self.x == other.x) and (self.y == other.y) and (self.z == other.z) and (self.rx == other.rx) and (self.ry == other.ry) and (self.rz == other.rz)
+            return (self.x == other.x) and (self.y == other.y) and (self.z == other.z)
+        return False
+    
+    def __str__(self):
+        return "X: " + str(self.x) + ", Y: " + str(self.y) + ", Z: " + str(self.z) + ", rX: " + str(self.rx) + ", rY: " + str(self.ry) + ", rZ: " + str(self.rz)
+
+
+def lerp(a: float, b: float, percent: float) -> float:
+    '''Takes two values and a percent between 0 and 1. Returns a liner interpolated value.'''
+    return a + ((b - a) * percent)
