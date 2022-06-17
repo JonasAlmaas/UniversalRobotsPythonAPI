64fe478f6c9d345a02a9e8f13ac2dcf0014c3086
JonasAlmaas <44512227+JonasAlmaas@users.noreply.github.com>
2022-06-17 09:19:16 +0200

    Create joint_pos_getter.py

---
+++ scripts/joint_pos_getter.py
@@ -0,0 +1,29 @@
+# Middle mouse to get pose
+# Left mouse to end program
+
+import os
+import sys
+
+from pynput.mouse import Listener, Button
+
+p = os.path.abspath('.')
+sys.path.insert(1, p)
+
+from urpy import urpy
+
+robot = urpy.UniversalRobot("192.168.1.101")
+
+robot.set_freedrive(state=True)
+
+def on_mouse_click(x, y, button, pressed):
+    if pressed:
+        if button == Button.middle:
+            pose = robot.get_joint_position()
+            print(pose.to_declaration())
+        
+        if button == Button.right:
+            robot.set_freedrive(state=False)
+            return False
+
+with Listener(on_click=on_mouse_click) as listener:
+    listener.join()
