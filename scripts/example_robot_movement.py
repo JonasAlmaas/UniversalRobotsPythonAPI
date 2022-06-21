b2c72c10ceedae2b8cf027f46ae69bca844b6137
JonasAlmaas <44512227+JonasAlmaas@users.noreply.github.com>
2022-06-21 08:20:39 +0200

    Update example_robot_movement.py

--- scripts/example_robot_movement.py
+++ scripts/example_robot_movement.py
@@ -2,18 +2,13 @@ import os
 import sys
 
 sys.path.insert(1, os.path.abspath('.'))
-from urpy import urpy
+
+from program.robot_interface.urpy import urpy
 
 
 class Pose:
-    pose1 = urpy.Pose(x=1060, y=-216, z=800, rx=-1.44, ry=1.02, rz=-1.56)
-    pose2 = urpy.Pose(x= 860, y=-216, z=1130, rx=2.4, ry=-1.7, rz=2.6)
-    # camera = urpy.Pose(1003, -180, 1244, -1.48, 1.0, -1.51)
-    # camera = urpy.Pose(853, -180, 1220, -1.48, 1.0, -1.51)
-    camera = urpy.Pose(x=870.0, y=-181.7, z=1215.4, rx=-1.49, ry=1.01, rz=-1.5)
-    
-    pmi_start_1 = urpy.Pose(x=659, y=-174, z=678, rx=-1.28, ry=1.21, rz=-1.17)
-    pmi_start_2 = urpy.Pose(x=1084, y=-161, z=486, rx=1.97, ry=-1.91, rz=-0.47)
+    example1 = urpy.Pose(x=870.0, y=-181.7, z=1215.4, rx=-1.49, ry=1.01, rz=-1.5)
+    example2 = urpy.Pose(x=700.0, y=-181.7, z=700.4, rx=-1.49, ry=1.01, rz=-1.5)
 
 
 class JointPosition:
@@ -21,17 +16,20 @@ class JointPosition:
     example2 = urpy.JointPosition(base=-0.615, shoulder=-2.839, elbow=-0.416, wrist1=-1.013, wrist2=2.589, wrist3=-0.395)
     example3 = urpy.JointPosition(base=0.523, shoulder=-2.514, elbow=-0.84, wrist1=-0.06, wrist2=1.556, wrist3=-0.395)
 
+
 robot = urpy.UniversalRobot("192.168.1.101")
 
-robot.move_to(target_pose=Pose.camera)
-
+robot.move_to_pose(target_pose=Pose.example1)
+robot.move_to_pose(target_pose=Pose.example2)
 robot.move_to_joint_pos(target=JointPosition.example1)
 robot.move_to_joint_pos(target=JointPosition.example2)
 robot.move_to_joint_pos(target=JointPosition.example3)
 
-# robot.move_to(target_pose=Pose.pmi_start_1)
-
-# robot.move_to(target_pose=Pose.pose2)
-# robot.move_to(target_pose=Pose.pose1)
-
-# robot.move_to(urpy.Pose(x=680, y=-200, z=400, rx=2.59, ry=-1.77, rz=0))
+path = [
+    urpy.PathPoint(Pose.example1),
+    urpy.PathPoint(Pose.example2),
+    urpy.PathPoint(JointPosition.example1),
+    urpy.PathPoint(JointPosition.example2),
+    urpy.PathPoint(JointPosition.example3)
+]
+robot.move_path(path_points=path)
